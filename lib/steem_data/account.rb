2780348727afd514fb8e7f8d9334d2f4acb4ee14
Anthony Martin <github@martin-studio.com>
2017-03-19 20:46:57 -0700

    Improved logic for finding proxied accounts.

--- lib/steem_data/account.rb
+++ lib/steem_data/account.rb
@@ -9,11 +9,35 @@ module SteemData
     def following_accounts; Account.where(:name.in => following); end
     def follower_accounts; Account.where(:name.in => followers); end
     def posts; Post.where(author: name); end
-    def proxy_accounts; Account.where(:name.in => proxies); end
-
-    def proxies
-      AccountOperation.type('account_witness_proxy').where(proxy: name).
-        distinct('account')
+    def proxied_accounts; Account.where(:name.in => proxied); end
+    
+    def proxied
+      proxied = []
+      
+      # List of accounts that have used this account as a proxy.
+      accounts = AccountOperation.type('account_witness_proxy').where(proxy: name).
+        distinct(:account) - [name]
+      
+      # Related operations (either voting for this account or resetting).
+      account_witness_proxies = AccountOperation.type('account_witness_proxy').
+        where(:account.in => accounts, :proxy.in => ['', name]).
+        order_by(timestamp: :asc)
+      
+      # Replay the proxy selection related to this account.  Note, this
+      # iteration will even work if the full proxy history is replayed, because
+      # we are only looking for "this account" or "not this account" in the
+      # order that those operations appear on the blockchain.
+      account_witness_proxies.each do |b|
+        if b.proxy == name
+          proxied += [b.account]
+        else
+          proxied -= [b.account]
+        end
+        
+        proxied = proxied.uniq
+      end
+      
+      proxied
     end
   end
 end
--- lib/steem_data/version.rb
+++ lib/steem_data/version.rb
@@ -1,3 +1,3 @@
 module SteemData
-  VERSION = '0.0.1b'
+  VERSION = '0.0.1c'
 end
